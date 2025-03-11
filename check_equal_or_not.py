431e257e39d5b8a71b244aefef0a01a7124df691
Shane <palomoshanecraven@gmail.com>
2025-03-11 20:05:22 +0800

    Add condition that will print 'equal' if the numbers are equal>

--- check_equal_or_not.py
+++ check_equal_or_not.py
@@ -5,3 +5,5 @@ number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the first number: "))
 
 #Validate if equal and print it
+if number_1 == number_2:
+    print("Equal")
