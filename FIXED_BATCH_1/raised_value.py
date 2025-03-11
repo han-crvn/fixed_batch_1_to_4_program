6148bae60b06a820133c22ed0aa4f2e51e9db26b
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:07:06 +0800

    Get the raised value and print it.

--- FIXED_BATCH_1/raised_value.py
+++ FIXED_BATCH_1/raised_value.py
@@ -5,5 +5,7 @@ number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the second number: "))
 
 #Raised the first number by the second number
+raised = number_1 ** number_2
 
 #Print the raised value
+print(f"Result: {raised}")
