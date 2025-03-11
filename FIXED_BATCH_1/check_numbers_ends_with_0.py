2d038186c03041bc8f7bbe68d86bf9159080c3cc
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:42:06 +0800

    Create a loop for 1 - 100 and validate them if they do not end with 0. Add them to the list and print the list.

--- FIXED_BATCH_1/check_numbers_ends_with_0.py
+++ FIXED_BATCH_1/check_numbers_ends_with_0.py
@@ -1,9 +1,14 @@
 #Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 
 #Create a empty list
+not_zero = []
 
 #Create a loop that will input 1 - 100
+for number in range(101):
 
-#Validate if they ends with 0 and append if they are not
+    #Validate if they ends with 0 and append if they are not
+    if number % 10 != 0:
+        not_zero.append(number)
 
 #Print the list
+print(not_zero)
