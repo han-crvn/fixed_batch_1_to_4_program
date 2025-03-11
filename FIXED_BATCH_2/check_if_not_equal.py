b7b577560681125430b18affe51f75ce0474f19f
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:57:22 +0800

    Add condition to check if the numbers are equal or not and print the result.

--- FIXED_BATCH_2/check_if_not_equal.py
+++ FIXED_BATCH_2/check_if_not_equal.py
@@ -5,3 +5,7 @@ number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the first number: "))
 
 #Validate the 2 numbers if they are not equal and print the result
+if number_1 != number_2:
+    print("Not Equal")
+else:
+    print("Equal")
