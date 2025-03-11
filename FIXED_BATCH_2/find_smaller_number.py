c02c02047d2b3ec3ef8d4830e889c8105b6f0606
Shane <palomoshanecraven@gmail.com>
2025-03-11 22:12:56 +0800

    Remove all the first line (instructions) in all programs.

--- FIXED_BATCH_2/find_smaller_number.py
+++ FIXED_BATCH_2/find_smaller_number.py
@@ -1,5 +1,3 @@
-#Create a program that ask user to input 2 numbers. Print the smaller number.
-
 #Input 2 numbers
 number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the first number: "))
