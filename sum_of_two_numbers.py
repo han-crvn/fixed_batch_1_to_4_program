31a6081d5cfcfd6e755db499778bbaeb5d8267d3
Shane <palomoshanecraven@gmail.com>
2025-03-11 20:46:18 +0800

    Add the two numbers and print the result.

--- sum_of_two_numbers.py
+++ sum_of_two_numbers.py
@@ -5,5 +5,7 @@ number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the second number: "))
 
 #Add the 2 numbers
+sum = number_1 + number_2
 
 #Print the result
+print(f"Result: {sum}")
