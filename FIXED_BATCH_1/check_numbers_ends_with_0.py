0dfaa75056f8c038d9cc43c4ce1bef982ba5519c
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:37:18 +0800

    Pseudocode for this program.

---
+++ FIXED_BATCH_1/check_numbers_ends_with_0.py
@@ -0,0 +1,9 @@
+#Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+#Create a empty list
+
+#Create a loop that will input 1 - 100
+
+#Validate if they ends with 0 and append if they are not
+
+#Print the list
