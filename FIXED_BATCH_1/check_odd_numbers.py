c10d13033043e504730d83b280070d012a2ef723
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:25:29 +0800

    Check if the number is odd and append it to the list(list_of_odds).

--- FIXED_BATCH_1/check_odd_numbers.py
+++ FIXED_BATCH_1/check_odd_numbers.py
@@ -1,12 +1,15 @@
 #Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
 #Create a empty list
-numbers = []
+list_of_odds = []
 
 #Create a loop the will allow users to input 10 numbers
 for i in range(10):
-    numbers = int(input(f"Enter number {i + 1}: "))
-    
-#Add a condition that will check and add the odd number to the list
+    number = int(input(f"Enter number {i + 1}: "))
+
+    #Add a condition that will check and add the odd number to the list
+    if number % 2 != 0:
+         list_of_odds.append(number)
 
 #Print the list of odd numbers
+print(f"The number of odd numbers is {len(list_of_odds)}.")
