d5767a9b99ee7f4e138c24d7d18afefa46ebf897
Shane <palomoshanecraven@gmail.com>
2025-03-11 20:03:22 +0800

    Allow users to input the numbers.

--- check_equal_or_not.py
+++ check_equal_or_not.py
@@ -1,5 +1,7 @@
 #Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 
 #Input the numbers
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the first number: "))
 
 #Validate if equal and print it
