e38128a4806d64d2c856c91caa9158d48d38b06c
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:51:07 +0800

    Add another condition if they are equal.

--- FIXED_BATCH_2/find_smaller_number.py
+++ FIXED_BATCH_2/find_smaller_number.py
@@ -9,3 +9,5 @@ if number_1 < number_2:
     print(f"{number_1} is the smaller number.") 
 elif number_2 < number_1:
     print(f"{number_2} is the smaller number.")
+else:
+    print("Both are equal.")
