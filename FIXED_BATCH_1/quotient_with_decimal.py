96290ff1453f786320974d836d0bc4d4b4ed76d1
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:01:01 +0800

    Pseudocode for this program.

---
+++ FIXED_BATCH_1/quotient_with_decimal.py
@@ -0,0 +1,7 @@
+#Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point.
+
+#Input 2 numbers
+
+#Get the quotient using the 2 numbers
+
+#Print the quotient
