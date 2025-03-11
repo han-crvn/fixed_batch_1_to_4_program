43c2cd04cc256fd08542c34c5539de73128ce79b
Shane <palomoshanecraven@gmail.com>
2025-03-11 23:14:33 +0800

    Validate if it is even and add it to the list. Then count the even numbers in the list and print it.

--- FIXED_BATCH_2/count_even_numbers.py
+++ FIXED_BATCH_2/count_even_numbers.py
@@ -6,5 +6,8 @@ for i in range(10):
     number = int(input(f"[{i + 1}] Enter the number: "))
 
 #Validate if it is a even number and add it to the list
+    if number % 2 == 0:
+        list_of_even.append(number)
 
 #Count the total number of even numbers
+print(f"The total even numbers in the list is {len(list_of_even)}.")
