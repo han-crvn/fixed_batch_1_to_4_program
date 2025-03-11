6793748f73544390b3a53c9e5a4240d58174c021
Shane <palomoshanecraven@gmail.com>
2025-03-11 22:12:40 +0800

    Remove all the first line (instructions) in all programs.

--- FIXED_BATCH_2/difference_of_two_numbers.py
+++ FIXED_BATCH_2/difference_of_two_numbers.py
@@ -1,5 +1,3 @@
-#Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
-
 #Input 2 numbers
 number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the first number: "))
