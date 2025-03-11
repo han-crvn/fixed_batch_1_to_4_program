722b52411c33c1240b0234f31507b5c9addc90be
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:53:32 +0800

    Pseudocode for this program.

---
+++ FIXED_BATCH_2/check_if_not_equal.py
@@ -0,0 +1,5 @@
+#Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+#Input 2 numbers
+
+#Validate the 2 numbers if they are not equal and print the result
