4c00c8901e9537fa1816cb977d3fe3c8249c47fd
Shane <palomoshanecraven@gmail.com>
2025-03-11 23:06:50 +0800

    Add total and set to 0, then add condition to manage adding and subtracting to total.

--- FIXED_BATCH_2/subtract_remaining.py
+++ FIXED_BATCH_2/subtract_remaining.py
@@ -1,9 +1,16 @@
+#Initialize total to 0
+total = 0
+
 #Use loop to input 10 numbers
 for i in range(10):
     number = float(input(f"[{i + 1}] Enter the number: "))
 
-#Add the first number
+    #Add the first number
+    if i == 0:
+        total += number
 
-# Subtract the remaining numbers
-
+    # Subtract the remaining numbers
+    else:
+        total -= number
+        
 #Print the result
