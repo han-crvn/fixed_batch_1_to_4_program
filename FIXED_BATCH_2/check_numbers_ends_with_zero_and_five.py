741ce1adaae8339c7a58a604df91b672ee103767
Shane <palomoshanecraven@gmail.com>
2025-03-11 23:25:17 +0800

    Applied all the steps in the pseudocode.

--- FIXED_BATCH_2/check_numbers_ends_with_zero_and_five.py
+++ FIXED_BATCH_2/check_numbers_ends_with_zero_and_five.py
@@ -1,7 +1,12 @@
 #Create empty list
+num_list = []
 
 #Use for loop to input 1 - 100
-
-#Validate if number ends with 0 or 5 and add if they are not
+for number in range(101):
+    
+    #Validate if number ends with 0 or 5 and add if they are not
+    if number % 5 != 0:
+        num_list.append(number)
 
 # Print result
+print(f"Result: {num_list}.")
