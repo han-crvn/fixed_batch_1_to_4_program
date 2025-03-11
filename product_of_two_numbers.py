23410c5f57197cc2fae3f47809f2935fc66e6694
Shane <palomoshanecraven@gmail.com>
2025-03-11 20:51:48 +0800

    Multiply the 2 numbers and print the product.

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -5,5 +5,7 @@ number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the second number: "))
 
 #Multiply the 2 numbers
+product = number_1 * number_2
 
 #Print the result
+print(f"Result: {product}")
