8f5d58932fff27c85578aa8e2ded42cbb6aebe97
Shane <palomoshanecraven@gmail.com>
2025-03-11 22:20:22 +0800

    Allow users to input 2 numbers.

--- FIXED_BATCH_2/remainder_of_numbers.py
+++ FIXED_BATCH_2/remainder_of_numbers.py
@@ -1,4 +1,6 @@
 #Input 2 numbers
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the first number: "))
 
 #Get the remainder of the first number using 2nd number
 
