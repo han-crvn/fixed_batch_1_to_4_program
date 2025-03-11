a34f410a3ad7a577d6b544c4a31ef043531de239
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:20:06 +0800

    Pseudocode for this program.

---
+++ FIXED_BATCH_1/check_odd_numbers.py
@@ -0,0 +1,9 @@
+#Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+#Create a empty list
+
+#Create a loop the will allow users to input 10 numbers
+
+#Add a condition that will check and add the odd number to the list
+
+#Print the list of odd numbers
