73a3d84586c8da52a66026f8e7c3dac83586ae17
Shane <palomoshanecraven@gmail.com>
2025-03-11 22:12:25 +0800

    Remove all the first line (instructions) in all programs.

--- FIXED_BATCH_2/check_if_not_equal.py
+++ FIXED_BATCH_2/check_if_not_equal.py
@@ -1,5 +1,3 @@
-#Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
-
 #Input 2 numbers
 number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the first number: "))
