9d39fb03e6f6ed586e76b610d42af8d0c63d4159
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:13:40 +0800

    Allow users to input 10 nunumber using for loop.

--- FIXED_BATCH_1/sum_of_ten_numbers.py
+++ FIXED_BATCH_1/sum_of_ten_numbers.py
@@ -1,9 +1,11 @@
 #Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 #Initialize the total to 0
+total = 0
 
 #Create a loop that will allow user to input 10 times
-
+for i in range(10):
+    numbers = float(input(f"Enter number {i + 1}: "))
 #Add the 10 numbers
 
 #Print the total sum
