16c5ddee3505b098612e282e5f2d61569877cb59
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:30:20 +0800

    Pseudocode for this program.

---
+++ FIXED_BATCH_1/check_even_numbers_up_to_100.py
@@ -0,0 +1,9 @@
+#Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+#Create a empty list
+
+#Create a for loop that will input up to 100
+
+#Validate the numbers if they are even and add it to the list
+
+#Print even numbers
