117ce9ab7e525b041701ef9c69e357505262ec40
Shane <palomoshanecraven@gmail.com>
2025-03-11 22:01:02 +0800

    Get the difference of two numbers and print the result.

--- FIXED_BATCH_2/difference_of_two_numbers.py
+++ FIXED_BATCH_2/difference_of_two_numbers.py
@@ -5,5 +5,7 @@ number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the first number: "))
 
 #Get the difference of the numberss
+difference = number_1 - number_2
 
 #Print the difference
+print(f"Result: {difference}")
