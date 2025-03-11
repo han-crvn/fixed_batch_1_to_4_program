430b6f011a5c4f31f3e9f6dbc81479b31f39ced7
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:50:10 +0800

    Add condition that will check which is smaller and print it.

--- FIXED_BATCH_2/find_smaller_number.py
+++ FIXED_BATCH_2/find_smaller_number.py
@@ -5,3 +5,7 @@ number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the first number: "))
 
 #Validate which is smaller and print it
+if number_1 < number_2:
+    print(f"{number_1} is the smaller number.") 
+elif number_2 < number_1:
+    print(f"{number_2} is the smaller number.")
