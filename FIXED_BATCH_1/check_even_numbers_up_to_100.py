e586d9fda02d3940f6d82aa3ee578d8d2a6e15ad
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:33:52 +0800

    Validate all the numbers from 0 to 100 if they are even numbers, add them to the list, and print the list.

--- FIXED_BATCH_1/check_even_numbers_up_to_100.py
+++ FIXED_BATCH_1/check_even_numbers_up_to_100.py
@@ -1,9 +1,14 @@
 #Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 
 #Create a empty list
+list_of_even = []
 
 #Create a for loop that will input up to 100
-
-#Validate the numbers if they are even and add it to the list
+for number in range(101):
+    
+    #Validate the numbers if they are even and add it to the list
+    if number % 2 == 0:
+        list_of_even.append(number)
 
 #Print even numbers
+print(f"Even numbers are {list_of_even}.")
