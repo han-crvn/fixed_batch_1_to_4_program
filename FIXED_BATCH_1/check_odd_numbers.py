f01a608fb1282131c7b2d487f79dbd1f2cf88b10
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:21:29 +0800

    Allow users to input 10 numbers using for loop.

--- FIXED_BATCH_1/check_odd_numbers.py
+++ FIXED_BATCH_1/check_odd_numbers.py
@@ -1,9 +1,12 @@
 #Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
 #Create a empty list
+numbers = []
 
 #Create a loop the will allow users to input 10 numbers
-
+for i in range(10):
+    numbers = int(input(f"Enter number {i + 1}: "))
+    
 #Add a condition that will check and add the odd number to the list
 
 #Print the list of odd numbers
