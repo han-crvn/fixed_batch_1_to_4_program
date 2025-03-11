9ab7ee8d3d1e3167dace2bd770f88e8372189daa
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:05:58 +0800

    Allow users to input two numbers.

--- FIXED_BATCH_1/raised_value.py
+++ FIXED_BATCH_1/raised_value.py
@@ -1,6 +1,8 @@
 #Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
 #Input 2 numbers
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
 
 #Raised the first number by the second number
 
