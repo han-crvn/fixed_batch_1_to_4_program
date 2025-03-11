b02453bd81c2266101819ef4a2534c6da850fdb0
Shane <palomoshanecraven@gmail.com>
2025-03-11 23:42:09 +0800

    Create empty list and allow users to input two numbers.

--- FIXED_BATCH_2/number_between_two_numbers.py
+++ FIXED_BATCH_2/number_between_two_numbers.py
@@ -1,7 +1,8 @@
 #Create empty list
-
+number_between = []
 #Input 2 numbers
-
+number_1 = int(input("Enter 1st number: "))
+number_2 = int(input("Enter 2nd number: "))
 #Check if there is a number between the two numbers
 
 #Check which is smaller between two numbers and using for loop add the number in between to the list and print it
