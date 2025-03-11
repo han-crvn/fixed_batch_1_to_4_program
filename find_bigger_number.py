2790408d78d33b3f774551fecb406f5ab199471a
Shane <palomoshanecraven@gmail.com>
2025-03-11 19:53:55 +0800

    Check which is greater and print it. Additionally, fixed the pseudocode to proper capitalization.

--- find_bigger_number.py
+++ find_bigger_number.py
@@ -1,9 +1,11 @@
 #Create a program that ask user to input 2 numbers. Print the bigger number.
 
-#INPUT THE NUMBER
+#Input the numbers
 number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the first number: "))
 
-#VALIDATE WHICH IS BIGGER
-
-#PRINT BIGGER NUMBER
+#Validate which is bigger and print it
+if number_1 > number_2:
+    print(F"Bigger number: {number_1}")
+elif number_2 > number_1:
+    print(F"Bigger number: {number_1}")
