6fc092e0ef01dad6165ab76b8b4473d26b35dffa
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:01:48 +0800

    Allow users to input 2 numbers.

--- FIXED_BATCH_1/quotient_with_decimal.py
+++ FIXED_BATCH_1/quotient_with_decimal.py
@@ -1,6 +1,8 @@
 #Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point.
 
 #Input 2 numbers
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
 
 #Get the quotient using the 2 numbers
 
