2c2adddae3fab3bc21e8767e1b7b967f13ca6f8f
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:54:11 +0800

    Allow users to input 2 numbers.

--- FIXED_BATCH_2/check_if_not_equal.py
+++ FIXED_BATCH_2/check_if_not_equal.py
@@ -1,5 +1,7 @@
 #Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
 #Input 2 numbers
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the first number: "))
 
 #Validate the 2 numbers if they are not equal and print the result
