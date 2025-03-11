1c8a39dd71351e4ab6458d776ff733b2f9334373
Shane <palomoshanecraven@gmail.com>
2025-03-11 23:44:58 +0800

    Check if there is a number between, which is greater, add the number in between in the list and print the list.

--- FIXED_BATCH_2/number_between_two_numbers.py
+++ FIXED_BATCH_2/number_between_two_numbers.py
@@ -1,9 +1,22 @@
 #Create empty list
 number_between = []
+
 #Input 2 numbers
 number_1 = int(input("Enter 1st number: "))
 number_2 = int(input("Enter 2nd number: "))
+
 #Check if there is a number between the two numbers
+if abs(number_1 - number_2) > 1:
+    
+    #Check which is smaller between two numbers and using for loop add the number in between to the list and print it
+    if number_1 < number_2:
+        for number in range(number_1 + 1, number_2):
+            number_between.append(number)
+    else:
+        for number in range(number_2 + 1, number_1):
+            number_between.append(number)
 
-#Check which is smaller between two numbers and using for loop add the number in between to the list and print it
+    print(f"The number between {number_1} and {number_2}: {number_between}")
 
+else:
+    print(f"There is no number between {number_1} and {number_2}.")
