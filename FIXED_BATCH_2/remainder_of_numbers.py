e895fc8d408cb7c2c440ae52922271141cd637cb
Shane <palomoshanecraven@gmail.com>
2025-03-11 22:21:32 +0800

    Get the remainder and print the result.

--- FIXED_BATCH_2/remainder_of_numbers.py
+++ FIXED_BATCH_2/remainder_of_numbers.py
@@ -3,5 +3,7 @@ number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the first number: "))
 
 #Get the remainder of the first number using 2nd number
+remainder = number_1 % number_2
 
 #Print the result
+print(f"Result: {remainder}")
