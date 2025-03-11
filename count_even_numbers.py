818a75fac9f8eb7e309e191797e9d6b99ab9ff2e
Shane <palomoshanecraven@gmail.com>
2025-03-11 23:11:53 +0800

    Allow users to input 10 numbers using for loop.

--- count_even_numbers.py
+++ count_even_numbers.py
@@ -1,6 +1,9 @@
 #Create empty list
+list_of_even = []
 
 #Create loop to input numbers for 10 times
+for i in range(10):
+    number = int(input(f"[{i + 1}] Enter the number: "))
 
 #Validate if it is a even number and add it to the list
 
