89bfd8f8578678befcc785d86189e19bea9dd2e1
Shane <palomoshanecraven@gmail.com>
2025-03-11 23:40:59 +0800

    Pseudocode for this program.

---
+++ FIXED_BATCH_2/number_between_two_numbers.py
@@ -0,0 +1,8 @@
+#Create empty list
+
+#Input 2 numbers
+
+#Check if there is a number between the two numbers
+
+#Check which is smaller between two numbers and using for loop add the number in between to the list and print it
+
