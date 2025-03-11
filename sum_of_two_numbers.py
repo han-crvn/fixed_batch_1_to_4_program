0f615ba3da27f31fb90da5537e51856c3a11aa3c
Shane <palomoshanecraven@gmail.com>
2025-03-11 20:44:34 +0800

    Allow users to input 2 numbers.

--- sum_of_two_numbers.py
+++ sum_of_two_numbers.py
@@ -1,6 +1,8 @@
 #Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
 #Input 2 numbers
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
 
 #Add the 2 numbers
 
