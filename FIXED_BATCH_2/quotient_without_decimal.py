313b5bf3a5fc0edfc77eaf43ec45929b87a10136
Shane <palomoshanecraven@gmail.com>
2025-03-11 22:17:30 +0800

    Get the quotient and print the result without decimal.

--- FIXED_BATCH_2/quotient_without_decimal.py
+++ FIXED_BATCH_2/quotient_without_decimal.py
@@ -3,5 +3,7 @@ number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the first number: "))
 
 #Get the quotient of the numbers
+quotient = number_1 //  number_2
 
 #Print the result without decimal
+print(f"Result: {quotient:.0f}")
