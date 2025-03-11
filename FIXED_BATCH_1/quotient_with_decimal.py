d0f5617baa8792b4cc13eb2b67998690bb115820
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:03:04 +0800

    Get the quotient of the 2 numbers annd print it.

--- FIXED_BATCH_1/quotient_with_decimal.py
+++ FIXED_BATCH_1/quotient_with_decimal.py
@@ -5,5 +5,7 @@ number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the second number: "))
 
 #Get the quotient using the 2 numbers
+product = number_1 / number_2
 
 #Print the quotient
+print(f"Result: {product}")
