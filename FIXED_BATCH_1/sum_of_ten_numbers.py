d11464ffa708e1ae76cb92cb5c577f5f6d2578b2
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:15:26 +0800

    Add the ten numbers and print the total sum.

--- FIXED_BATCH_1/sum_of_ten_numbers.py
+++ FIXED_BATCH_1/sum_of_ten_numbers.py
@@ -6,6 +6,9 @@ total = 0
 #Create a loop that will allow user to input 10 times
 for i in range(10):
     numbers = float(input(f"Enter number {i + 1}: "))
-#Add the 10 numbers
+    
+    #Add the 10 numbers
+    total += numbers
 
 #Print the total sum
+print(f"Total sum: {total}")
