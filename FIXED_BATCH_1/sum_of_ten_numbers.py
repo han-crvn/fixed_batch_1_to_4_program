87974a8562ddc6ae7b72eaad77499a5125812e10
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:11:56 +0800

    Pseudocode for this program.

---
+++ FIXED_BATCH_1/sum_of_ten_numbers.py
@@ -0,0 +1,9 @@
+#Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+#Initialize the total to 0
+
+#Create a loop that will allow user to input 10 times
+
+#Add the 10 numbers
+
+#Print the total sum
