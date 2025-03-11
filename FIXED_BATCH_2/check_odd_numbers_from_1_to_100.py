79af739c03dab0a980a3767b0fb7b40808ec1943
Shane <palomoshanecraven@gmail.com>
2025-03-11 23:19:57 +0800

    Applied all the steps in the pseudocode.

--- FIXED_BATCH_2/check_odd_numbers_from_1_to_100.py
+++ FIXED_BATCH_2/check_odd_numbers_from_1_to_100.py
@@ -1,9 +1,16 @@
 #Initialize count to 0 and create a list
+count = 0
+num_list = []
 
 #Create a while loop
-
-#Validate if number is odd and add it to the list
+while count < 101:
+    
+    #Validate if number is odd and add it to the list
+    if count % 2 != 0:
+        num_list.append(count)
 
 #Update the count
+    count += 1
 
 #Print the result
+print(f"Result: {num_list}.")
