290ef12b1f45699c9b242d752f56fe4c120e7eef
Shane <palomoshanecraven@gmail.com>
2025-03-11 21:05:14 +0800

    Pseudocode for this program.

---
+++ FIXED_BATCH_1/raised_value.py
@@ -0,0 +1,7 @@
+#Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+#Input 2 numbers
+
+#Raised the first number by the second number
+
+#Print the raised value
