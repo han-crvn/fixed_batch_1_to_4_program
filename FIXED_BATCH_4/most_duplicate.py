30a97a15397c36da9bc0cc7c747ad56a065f848e
Shane <palomoshanecraven@gmail.com>
2025-03-12 11:29:12 +0800

    Algorithm for this program.

---
+++ FIXED_BATCH_4/most_duplicate.py
@@ -0,0 +1,9 @@
+#Create a list
+
+#Use loop and try-except to allow users to input number and stop if the input is invalid
+
+#Update the list
+
+#Check which number has the most occurences
+
+#Print the result
