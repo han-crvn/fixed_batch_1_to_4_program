f057bc6b5a5fd865a03793bcab151913b9d66d3c
Shane <palomoshanecraven@gmail.com>
2025-03-12 10:59:11 +0800

    Allow users to input numbers as much as they want.

--- FIXED_BATCH_3/minimum_number.py
+++ FIXED_BATCH_3/minimum_number.py
@@ -1,7 +1,16 @@
 #Create a list
+numbers_list = []
 
 #Use loop and try-except to allow users to input number and stop if the input is invalid
-
-#Update the list
+while True:
+    try:
+        number = int(input("Enter the number: "))
+        
+        #Update the list
+        numbers_list.append(number)
 
 #Check which is the smallest number and print it
+    
+    except ValueError:
+        print("Invalid")
+        break
