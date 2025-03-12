47d7ca9050d442b158e6a07f631c3fc90c3c2545
Shane <palomoshanecraven@gmail.com>
2025-03-12 10:11:19 +0800

    Use another loop to check the occurences of numbers and add it to to a new list. Then, print the result.

--- FIXED_BATCH_3/non_duplicate_numbers.py
+++ FIXED_BATCH_3/non_duplicate_numbers.py
@@ -1,14 +1,18 @@
-#Create an empty list
+#Create an empty lists
 numbers_list =[]
+single_numbers = []
 
 #Use for loop to allow user to input 10 numbers
 for i in range(10):
     number = int(input(f"[{i + 1}] Enter the number: "))
+    
+    #Add the numbers in the list
     numbers_list.append(number)
 
-#Add the numbers in the list
-
 #Use loop to check the occurences of numbers
-
+for number in numbers_list:
+    if numbers_list.count(number) == 1:
+        single_numbers.append(number)
+        
 #Print the result
-print(numbers_list)
+print(f"Result: {single_numbers}")
