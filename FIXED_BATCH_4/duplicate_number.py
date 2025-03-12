166434b63b9a1d3988a03e12553c0595c67114e9
Shane <palomoshanecraven@gmail.com>
2025-03-12 11:23:32 +0800

    Add the number to the list, validate them if they have duplicate, add the duplicate to another list, and print the result.

--- FIXED_BATCH_4/duplicate_number.py
+++ FIXED_BATCH_4/duplicate_number.py
@@ -5,9 +5,14 @@ duplicate_numbers = []
 #Use for loop to allow user to input 10 numbers
 for i in range(10):
     number = int(input(f"[{i + 1}] Enter the number: "))
-    
-#Add the numbers in the list
+
+    #Add the numbers in the list
+    numbers_list.append(number)
 
 #Use loop to check the occurences of numbers
+for number in numbers_list:
+    if numbers_list.count(number) != 1:
+        duplicate_numbers.append(number)
 
 #Print the result
+print(f"Result: {duplicate_numbers}")
