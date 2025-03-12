bd293cb9f7265ad2700d42d61b92609f41a401ec
Shane <palomoshanecraven@gmail.com>
2025-03-12 10:33:41 +0800

    Allow users to input 10 numbers using loop.

--- FIXED_BATCH_3/no_duplicate_numbers.py
+++ FIXED_BATCH_3/no_duplicate_numbers.py
@@ -1,8 +1,14 @@
 #Create a empty list
+number_list = []
 
 #Use for loop to allow users to input 10 numbers
+for i in range(10):
+    number = int(input(f"[{i + 1}] Enter the number: "))
 
 #Check if the number are not in the list and add them
 
 #Print result
 
+
+
+  
