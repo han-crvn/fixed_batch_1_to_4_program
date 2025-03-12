832b50df3470c41e6f6aa84313e427e156c3ee03
Shane <palomoshanecraven@gmail.com>
2025-03-12 11:56:42 +0800

    Algorithm for this program.

---
+++ FIXED_BATCH_4/highest_to_lowest.py
@@ -0,0 +1,9 @@
+#Create empty list
+
+#Use loop and try-except to allow users to input number and stop if the input is invalid
+
+#Update the list
+
+#Arrange from highest to lowest
+
+#Print the result
