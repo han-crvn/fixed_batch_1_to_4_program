2588fbf2fa854fa8b74e3e9580cd8354835782d8
Shane <palomoshanecraven@gmail.com>
2025-03-12 14:11:53 +0800

    Fixed comments.

--- FIXED_BATCH_1/check_numbers_ends_with_0.py
+++ FIXED_BATCH_1/check_numbers_ends_with_0.py
@@ -1,12 +1,12 @@
-#Create a empty list
+# Create a empty list
 not_zero = []
 
-#Create a loop that will input 1 - 100
+# Create a loop that will input 1 - 100
 for number in range(101):
 
-    #Validate if they ends with 0 and append if they are not
+    # Validate if they ends with 0 and append if they are not
     if number % 10 != 0:
         not_zero.append(number)
 
-#Print the list
+# Print the list
 print(not_zero)
