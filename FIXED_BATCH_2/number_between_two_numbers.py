3191965e32c7e90e4970cd77580594823b282779
Shane <palomoshanecraven@gmail.com>
2025-03-12 13:56:20 +0800

    Fixed comments.

--- FIXED_BATCH_2/number_between_two_numbers.py
+++ FIXED_BATCH_2/number_between_two_numbers.py
@@ -1,14 +1,14 @@
-#Create empty list
+# Create empty list
 number_between = []
 
-#Input 2 numbers
+# Input 2 numbers
 number_1 = int(input("Enter 1st number: "))
 number_2 = int(input("Enter 2nd number: "))
 
-#Check if there is a number between the two numbers
+# Check if there is a number between the two numbers
 if abs(number_1 - number_2) > 1:
     
-    #Check which is smaller between two numbers and using for loop add the number in between to the list and print it
+    # Check which is smaller between two numbers and using for loop add the number in between to the list and print it
     if number_1 < number_2:
         for number in range(number_1 + 1, number_2):
             number_between.append(number)
