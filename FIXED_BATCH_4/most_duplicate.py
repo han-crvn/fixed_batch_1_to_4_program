29585ae377774d11e073709b2215cde82db13604
Shane <palomoshanecraven@gmail.com>
2025-03-12 11:30:21 +0800

    Allow users to input number as much as they want.

--- FIXED_BATCH_4/most_duplicate.py
+++ FIXED_BATCH_4/most_duplicate.py
@@ -1,9 +1,16 @@
 #Create a list
+numbers_list = []
 
 #Use loop and try-except to allow users to input number and stop if the input is invalid
-
+while True:
+    try:
+        number = int(input("Enter the number: "))
 #Update the list
 
 #Check which number has the most occurences
 
 #Print the result
+
+    except ValueError:
+        print("Invalid")
+        break
