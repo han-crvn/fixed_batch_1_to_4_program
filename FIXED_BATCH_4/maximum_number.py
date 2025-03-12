1c5117e2c9012003de9c1cdd925c598d316314b4
Shane <palomoshanecraven@gmail.com>
2025-03-12 11:50:10 +0800

    Allow users to input number as muuch as they want.

--- FIXED_BATCH_4/maximum_number.py
+++ FIXED_BATCH_4/maximum_number.py
@@ -1,9 +1,16 @@
 #Create a list
+numbers_list = []
 
 #Use for loop to allow users to input 10 numbers
-
+while True:
+    try:
+        number = int(input("Enter the number: "))
 #Update the list
 
 #Check which is the highest number
 
 #Print the result
+
+    except ValueError:
+        print("Invalid")
+        break
