61e9c6bd17d80048650470638b2082e63983ff03
Shane <palomoshanecraven@gmail.com>
2025-03-12 13:39:10 +0800

    Fixed comment.

--- FIXED_BATCH_4/most_duplicate.py
+++ FIXED_BATCH_4/most_duplicate.py
@@ -1,21 +1,21 @@
-#Create a list
+# Create a lists and variable
 numbers_list = []
 most_number = []
 max_count = 0
 
-#Use loop and try-except to allow users to input number and stop if the input is invalid
+# Use loop and try-except to allow users to input number and stop if the input is invalid
 while True:
     try:
         number = float(input("Enter the number: "))
 
-        #Update the list
+        # Update the list
         numbers_list.append(number)
 
     except ValueError:
         print("Invalid")
         break
 
-#Check which number has the most occurences
+# Check which number has the most occurences
 for number in numbers_list:
         count = numbers_list.count(number)
         if count > max_count:
@@ -24,5 +24,5 @@ for number in numbers_list:
         elif count == max_count and number not in most_frequent:
             most_frequent.append(number)
 
-#Print the result
+# Print the result
 print(f"Results: {most_frequent} (occurs: {max_count})")
