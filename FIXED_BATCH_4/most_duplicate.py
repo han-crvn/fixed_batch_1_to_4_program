6a5a41dcffacbdeeaae8854b6e2fcd7ca81c23d8
Shane <palomoshanecraven@gmail.com>
2025-03-12 11:45:49 +0800

    Added another variables to store data, validate which number has the most occurences, and print the result.

--- FIXED_BATCH_4/most_duplicate.py
+++ FIXED_BATCH_4/most_duplicate.py
@@ -1,15 +1,23 @@
 #Create a list
 numbers_list = []
+most_number = None
+max_count = 0
 
 #Use loop and try-except to allow users to input number and stop if the input is invalid
 while True:
     try:
         number = int(input("Enter the number: "))
-#Update the list
 
-#Check which number has the most occurences
+        #Update the list
+        numbers_list.append(number)
 
-#Print the result
+        #Check which number has the most occurences
+        if numbers_list.count(number) > max_count:
+            max_count = numbers_list.count(number)
+            most_number = number
+
+        #Print the result
+        print(f"Result: {most_number} = {max_count}")
 
     except ValueError:
         print("Invalid")
