8ff361273e01460e86a5cef4eeb49859b9080b5c
Shane <palomoshanecraven@gmail.com>
2025-03-12 14:02:11 +0800

    Fixed typo in the program.

--- FIXED_BATCH_2/quotient_without_decimal.py
+++ FIXED_BATCH_2/quotient_without_decimal.py
@@ -1,6 +1,6 @@
 # Input 2 numbers
 number_1 = float(input("Enter the first number: "))
-number_2 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
 
 # Get the quotient of the numbers
 quotient = number_1 //  number_2
