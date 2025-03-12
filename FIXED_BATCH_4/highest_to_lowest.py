79e8369615b59e4c2da447989863fb51e6b42c35
Shane <palomoshanecraven@gmail.com>
2025-03-12 11:58:06 +0800

    Allow users to input numbers as much as they want.

--- FIXED_BATCH_4/highest_to_lowest.py
+++ FIXED_BATCH_4/highest_to_lowest.py
@@ -1,9 +1,17 @@
 #Create empty list
+numbers_list = []
 
 #Use loop and try-except to allow users to input number and stop if the input is invalid
+while True:
+    try:
+        number = int(input("Enter the number: "))
 
 #Update the list
 
 #Arrange from highest to lowest
 
 #Print the result
+    
+    except ValueError:
+        print("Invalid")
+        break
