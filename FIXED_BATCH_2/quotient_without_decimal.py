605c5f480e43b2b88f5abafd75322e401e6811f3
Shane <palomoshanecraven@gmail.com>
2025-03-12 13:53:40 +0800

    Fixed comments.

--- FIXED_BATCH_2/quotient_without_decimal.py
+++ FIXED_BATCH_2/quotient_without_decimal.py
@@ -1,9 +1,9 @@
-#Input 2 numbers
+# Input 2 numbers
 number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the first number: "))
 
-#Get the quotient of the numbers
+# Get the quotient of the numbers
 quotient = number_1 //  number_2
 
-#Print the result without decimal
+# Print the result without decimal
 print(f"Result: {quotient:.0f}")
