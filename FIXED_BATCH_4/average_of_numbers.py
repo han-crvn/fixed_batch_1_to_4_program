9caccf65cb2ee75dfbbc035066d1531a65071585
Shane <palomoshanecraven@gmail.com>
2025-03-12 12:10:19 +0800

    Algorithm for this program.

---
+++ FIXED_BATCH_4/average_of_numbers.py
@@ -0,0 +1,9 @@
+#Create an empty lists
+
+#Use loop and try-except to allow users to input number and stop if the input is invalid
+
+#Add the number to the list
+
+#Calculate the average of the numbers in the list
+
+#Print the result
