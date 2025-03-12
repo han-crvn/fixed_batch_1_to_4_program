4a029effc90dbbe07a1cde70a3794a7419285571
Shane <palomoshanecraven@gmail.com>
2025-03-12 12:22:49 +0800

    Fixed minor logic error.

--- FIXED_BATCH_3/lowest_to_highest.py
+++ FIXED_BATCH_3/lowest_to_highest.py
@@ -9,12 +9,12 @@ while True:
         #Update the list
         numbers_list.append(number)
 
-        #Arrange the list from lowest to highest
-        arranged_number = sorted(numbers_list)
-        
-        #Print the result
-        print(f"Result: {arranged_number}")
-
     except ValueError:
         print("Invalid")
         break
+
+#Arrange the list from lowest to highest
+arranged_number = sorted(numbers_list)
+        
+#Print the result
+print(f"Result: {arranged_number}")
