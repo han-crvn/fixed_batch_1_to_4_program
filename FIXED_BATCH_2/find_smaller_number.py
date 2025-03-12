b5b4171c72dbbc73042583c2b613a5bdbc77acb2
Shane <palomoshanecraven@gmail.com>
2025-03-12 14:00:54 +0800

    Fixed typo in the program.

--- FIXED_BATCH_2/find_smaller_number.py
+++ FIXED_BATCH_2/find_smaller_number.py
@@ -1,6 +1,6 @@
 # Input 2 numbers
 number_1 = float(input("Enter the first number: "))
-number_2 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
 
 # Validate which is smaller and print it
 if number_1 < number_2:
