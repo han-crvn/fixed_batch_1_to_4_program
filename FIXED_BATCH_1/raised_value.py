6d1b58bd3017ddd8cde743b37c07cc0e4990ea6b
Shane <palomoshanecraven@gmail.com>
2025-03-12 14:15:28 +0800

    Fixed comments.

--- FIXED_BATCH_1/raised_value.py
+++ FIXED_BATCH_1/raised_value.py
@@ -1,9 +1,9 @@
-#Input 2 numbers
+# Input 2 numbers
 number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the second number: "))
 
-#Raised the first number by the second number
+# Raised the first number by the second number
 raised = number_1 ** number_2
 
-#Print the raised value
+# Print the raised value
 print(f"Result: {raised}")
