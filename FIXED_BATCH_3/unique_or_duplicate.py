ed31c788df2ba1a13a0e31d131008c8facc1a571
Shane <palomoshanecraven@gmail.com>
2025-03-12 10:47:26 +0800

    Algorithm for this program.

---
+++ FIXED_BATCH_3/unique_or_duplicate.py
@@ -0,0 +1,7 @@
+#Create empty list
+
+#Use loop and try-except to allow users to input number and stop if the input is invalid
+
+#Validate if the number is in the list and print the result
+
+#Add the number to update the list
