4e819249122340a8b1bb3b47eb6bd51f3abaf373
Shane <palomoshanecraven@gmail.com>
2025-03-12 14:07:08 +0800

    Fixed comments.

--- FIXED_BATCH_2/check_numbers_ends_with_zero_and_five.py
+++ FIXED_BATCH_2/check_numbers_ends_with_zero_and_five.py
@@ -4,7 +4,7 @@ num_list = []
 # Use for loop to input 1 - 100
 for number in range(101):
     
-    #  if number ends with 0 or 5 and add if they are not
+    # Validate if number ends with 0 or 5 and add if they are not
     if number % 5 != 0:
         num_list.append(number)
 
