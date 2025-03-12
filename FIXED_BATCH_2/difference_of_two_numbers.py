73674693100d62237514d9e5a77a3b7e5023bf82
Shane <palomoshanecraven@gmail.com>
2025-03-12 14:03:00 +0800

    Fixed comment and typo in the program.

--- FIXED_BATCH_2/difference_of_two_numbers.py
+++ FIXED_BATCH_2/difference_of_two_numbers.py
@@ -1,9 +1,9 @@
-#Input 2 numbers
+# Input 2 numbers
 number_1 = float(input("Enter the first number: "))
-number_2 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
 
-#Get the difference of the numberss
+# Get the difference of the numberss
 difference = number_1 - number_2
 
-#Print the difference
+# Print the difference
 print(f"Result: {difference}")
