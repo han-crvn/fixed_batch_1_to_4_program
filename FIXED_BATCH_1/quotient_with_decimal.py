0a698fea0e98abbbeb3e70dd4f1a3ea934e8cf8f
Shane <palomoshanecraven@gmail.com>
2025-03-12 14:14:48 +0800

    Fixed comments.

--- FIXED_BATCH_1/quotient_with_decimal.py
+++ FIXED_BATCH_1/quotient_with_decimal.py
@@ -1,9 +1,9 @@
-#Input 2 numbers
+# Input 2 numbers
 number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the second number: "))
 
-#Get the quotient using the 2 numbers
+# Get the quotient using the 2 numbers
 product = number_1 / number_2
 
-#Print the quotient
+# Print the quotient
 print(f"Result: {product}")
