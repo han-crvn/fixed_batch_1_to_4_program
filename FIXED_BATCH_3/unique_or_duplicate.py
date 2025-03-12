9efb4a5e8ba64141bb8fe886b3f7ac96696b5a97
Shane <palomoshanecraven@gmail.com>
2025-03-12 10:51:20 +0800

    Applied the algorithm for  this program.

--- FIXED_BATCH_3/unique_or_duplicate.py
+++ FIXED_BATCH_3/unique_or_duplicate.py
@@ -1,7 +1,20 @@
 #Create empty list
+numbers_list = []
 
 #Use loop and try-except to allow users to input number and stop if the input is invalid
+while True:
+    try:
+        number = int(input("Enter the number: "))
+        
+        #Validate if the number is in the list and print the result
+        if number not in numbers_list:
+            print("Unique")
+        else:
+            print("Duplicate")
+        
+        #Add the number to update the list
+        numbers_list.append(number)
 
-#Validate if the number is in the list and print the result
-
-#Add the number to update the list
+    except ValueError:
+        print("Invalid")
+        break
