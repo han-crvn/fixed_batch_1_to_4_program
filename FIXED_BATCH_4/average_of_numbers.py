f7d42f98c71555e15f6ba2df5956a2fe48ec6bfa
Shane <palomoshanecraven@gmail.com>
2025-03-12 12:12:12 +0800

    Allow users to input numbers as much as they want.

--- FIXED_BATCH_4/average_of_numbers.py
+++ FIXED_BATCH_4/average_of_numbers.py
@@ -1,9 +1,17 @@
 #Create an empty lists
+numbers_list = []
 
 #Use loop and try-except to allow users to input number and stop if the input is invalid
+while True:
+    try:
+        number = int(input("Enter the number: "))
 
 #Add the number to the list
 
 #Calculate the average of the numbers in the list
 
 #Print the result
+
+    except ValueError:
+        print("Invalid")
+        break
