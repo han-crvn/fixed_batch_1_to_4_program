0b294cc50bba0a9520be146743d96f405f13745b
Shane <palomoshanecraven@gmail.com>
2025-03-12 14:11:16 +0800

    Fixed comments.

--- FIXED_BATCH_1/check_even_numbers_up_to_100.py
+++ FIXED_BATCH_1/check_even_numbers_up_to_100.py
@@ -1,12 +1,12 @@
-#Create a empty list
+# Create a empty list
 list_of_even = []
 
-#Create a for loop that will input up to 100
+# Create a for loop that will input up to 100
 for number in range(101):
     
-    #Validate the numbers if they are even and add it to the list
+    # Validate the numbers if they are even and add it to the list
     if number % 2 == 0:
         list_of_even.append(number)
 
-#Print even numbers
+# Print even numbers
 print(f"Even numbers are {list_of_even}.")
