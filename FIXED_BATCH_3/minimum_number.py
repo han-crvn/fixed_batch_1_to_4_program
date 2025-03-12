65440b27e555405f2d4e33d83be73f9fb80873c7
Shane <palomoshanecraven@gmail.com>
2025-03-12 11:54:00 +0800

    fix minor typo.

--- FIXED_BATCH_3/minimum_number.py
+++ FIXED_BATCH_3/minimum_number.py
@@ -9,9 +9,10 @@ while True:
         #Update the list
         numbers_list.append(number)
 
-        #Check which is the smallest number and print it
+        #Check which is the smallest number
         min_number = min(numbers_list)
         
+        #Print the result
         print(f"Result: {min_number}")
 
     except ValueError:
