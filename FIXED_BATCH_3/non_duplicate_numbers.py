6a85193ce00e43901e0b5983d744c60016a805e3
Shane <palomoshanecraven@gmail.com>
2025-03-12 10:06:28 +0800

    Allow users to input 10 numbers.

--- FIXED_BATCH_3/non_duplicate_numbers.py
+++ FIXED_BATCH_3/non_duplicate_numbers.py
@@ -1,9 +1,14 @@
 #Create an empty list
+numbers_list =[]
 
 #Use for loop to allow user to input 10 numbers
+for i in range(10):
+    number = int(input(f"[{i + 1}] Enter the number: "))
+    numbers_list.append(number)
 
 #Add the numbers in the list
 
 #Use loop to check the occurences of numbers
 
 #Print the result
+print(numbers_list)
