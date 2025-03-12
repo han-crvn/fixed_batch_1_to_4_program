f313098765d8bdd3a37d13c2af77a03e0904f921
Shane <palomoshanecraven@gmail.com>
2025-03-12 14:13:53 +0800

    Fixed comments and typo in the program.

--- FIXED_BATCH_1/find_bigger_number.py
+++ FIXED_BATCH_1/find_bigger_number.py
@@ -1,11 +1,11 @@
 #Input the numbers
 number_1 = float(input("Enter the first number: "))
-number_2 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
 
 #Validate which is bigger and print it
 if number_1 > number_2:
-    print(F"Bigger number: {number_1}")
+    print(f"Bigger number: {number_1}")
 elif number_2 > number_1:
-    print(F"Bigger number: {number_1}")
+    print(f"Bigger number: {number_2}")
 else:
     print("They are both equal")
