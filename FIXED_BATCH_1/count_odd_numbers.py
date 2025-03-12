d7f20266e979e944353264c06e8afffdc9fc61b0
Shane <palomoshanecraven@gmail.com>
2025-03-12 14:12:30 +0800

    Fixed comments.

--- FIXED_BATCH_1/count_odd_numbers.py
+++ FIXED_BATCH_1/count_odd_numbers.py
@@ -1,13 +1,13 @@
-#Create a empty list
+# Create a empty list
 list_of_odds = []
 
-#Create a loop the will allow users to input 10 numbers
+# Create a loop the will allow users to input 10 numbers
 for i in range(10):
     number = int(input(f"Enter number {i + 1}: "))
 
-    #Add a condition that will check and add the odd number to the list
+    # Add a condition that will check and add the odd number to the list
     if number % 2 != 0:
          list_of_odds.append(number)
 
-#Print the number of odd numbers in the list
+# Print the number of odd numbers in the list
 print(f"The number of odd numbers is {len(list_of_odds)}.")
