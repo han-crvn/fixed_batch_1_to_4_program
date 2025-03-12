874156c28d5b9edd094a868333e90fc5549a0ba5
Shane <palomoshanecraven@gmail.com>
2025-03-12 14:16:17 +0800

    Fixed comments.

--- FIXED_BATCH_1/sum_of_two_numbers.py
+++ FIXED_BATCH_1/sum_of_two_numbers.py
@@ -1,9 +1,9 @@
-#Input 2 numbers
+# Input 2 numbers
 number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the second number: "))
 
-#Add the 2 numbers
+# Add the 2 numbers
 sum = number_1 + number_2
 
-#Print the result
+# Print the result
 print(f"Result: {sum}")
